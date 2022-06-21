b26bf605efcaf68fe57a18d9eaaab8825f945de5
Harika_Kommuri <harikakommuri35@gmail.com>
2022-06-21 14:20:25 +0200

    Added Footer

--- d2-index/src/App.js
+++ d2-index/src/App.js
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from './Components/MyNav';
+import MyFooter from "./Components/MyFooter";
 
 function App() {
   return (
-    <MyNav />
+    <>
+      <MyNav />
+      <MyFooter />
+    </>
   );
 }
 
---
+++ d2-index/src/Components/MyFooter.jsx
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import { Card, CardGroup } from 'react-bootstrap'
+
+class MyFooter extends Component {
+    render() {
+        return (
+            <CardGroup>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>About Us</Card.Title>
+                        <a href="#">Books</a><br></br>
+                        <a href="#">Authors</a>
+                        <Card.Text>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Useful Links</Card.Title>
+                        <a href="#">Link 1</a><br></br>
+                        <a href="#">Link 2</a>
+                        <Card.Text>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Contact Us</Card.Title>
+                        <a href="#">Email</a><br></br>
+                        <a href="#">Phone</a>
+                        <Card.Text>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+        )
+    }
+}
+
+export default MyFooter;
